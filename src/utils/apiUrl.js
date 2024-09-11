c1f60206fd8f73d842f69bc5e464c4e1ee3938e9
BY1502 <sks14502@gmail.com>
2024-09-11 10:54:51 +0900

    commit

--- src/utils/apiUrl.js
+++ src/utils/apiUrl.js
@@ -1,9 +1,9 @@
-const GET_TASKS_API_URL = 'http://taskbackend.gunu110.com/get_tasks';
-const POST_TASKS_API_URL = 'http://taskbackend.gunu110.com/post_task';
-const DELETE_TASKS_API_URL = 'http://taskbackend.gunu110.com/delete_task';
+const GET_TASKS_API_URL = 'https://taskbackend.gunu110.com/get_tasks';
+const POST_TASKS_API_URL = 'https://taskbackend.gunu110.com/post_task';
+const DELETE_TASKS_API_URL = 'https://taskbackend.gunu110.com/delete_task';
 const UPDATE_COMPLETED_TASKS_API_URL =
-  'http://taskbackend.gunu110.com/update_completed_task';
-const UPDATE_TASK_API_URL = 'http://taskbackend.gunu110.com/update_task';
+  'https://taskbackend.gunu110.com/update_completed_task';
+const UPDATE_TASK_API_URL = 'https://taskbackend.gunu110.com/update_task';
 
 export {
   GET_TASKS_API_URL,
